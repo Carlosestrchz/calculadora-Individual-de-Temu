b18b4b2f98fbf12a63dc30846563a73c86f53f35
Carlos Ignacio Estrada Chairez <carlosestrchz@gmail.com>
2026-09-15 10:55:11 -0600

    develop: se crean funciones preliminares dentro del main

---
+++ main.py
@@ -0,0 +1,17 @@
+#funciones aritmeticas de una vez :D
+def suma(a,b):
+    return (a+b)
+
+def resta(a,b):
+    return (a-b)
+
+def multip(a,b):
+    return (a*b)
+
+def divis(a,b):
+    return (a/b)
+
+def modul(a,b):
+    return (a%b)
+
+def calculadora():
