6b6ff43669283b30d28dd33c037ea0822619012b
Carlos Ignacio Estrada Chairez <carlosestrchz@gmail.com>
2026-09-15 11:19:09 -0600

    develop: se desarrolla primera versión lógica del diagrama de flujo

--- main.py
+++ main.py
@@ -15,3 +15,52 @@ def modul(a,b):
     return (a%b)
 
 def calculadora():
+    print("====CALCULADORA MAS BASICA DEL MUNDO====\n")
+
+    #creo un acumulador para ir guardando el resultado
+    resultado = float(input("Introduce un valor por favor: "))
+
+
+    #bucle infinito para que pida siempre un valor hasta que no lo rompa el usuario
+    while True:
+        operador = input("OPERADORES: '+' para sumar, '-' para restar, '*' para multiplicar, '/' para dividir, '%' para modulo, o '=' para obtener resultado. \n Introduzca operación: ")
+
+
+        if operador == '=':
+            break
+
+        #validamos la entrada
+        if operador not in ('+','-','*','/','%'):
+            print("Operación desconocida, por favor, intente de nuevo\n")
+            continue
+
+        siguienteNum = float(input("Introduce el siguiente valor: "))
+
+        #aplicamos la logica aritmetica y de control que se ocupa
+        if operador == '+':
+            resultado = suma(resultado, siguienteNum)
+
+        elif operador == '-':
+            resultado = resta(resultado, siguienteNum)
+
+        elif operador == '*':
+            resultado == multip(resultado, siguienteNum)
+
+        elif operador == '/':
+            if siguienteNum == 0:
+                print("Error. No se puede dividir entre cero.\n")
+                continue
+
+            resultado == divis(resultado, siguienteNum)
+
+        elif operador == '%':
+            resultado = modul(resultado, siguienteNum)
+
+        print(f"Subtotal actual: {resultado}")
+
+    print(f"\nResultado final: {resultado}")
+
+
+#arrancamos la funcion calculadora
+calculadora()
+    
